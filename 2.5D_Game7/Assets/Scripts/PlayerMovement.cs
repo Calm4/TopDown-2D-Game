18dbeaea5ac440c7dfc2f20e248e6cecee08cd68
Grandpa Newton <ivan.gromyko.2804@gmail.com>
2023-01-28 17:40:02 +0300

    fixing moving

--- 2.5D_Game7/Assets/Scripts/PlayerMovement.cs
+++ 2.5D_Game7/Assets/Scripts/PlayerMovement.cs
@@ -19,7 +19,7 @@ public class PlayerMovement : MonoBehaviour
         transform.position = transform.position + movement * Time.deltaTime * speed;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Wall")
         {
