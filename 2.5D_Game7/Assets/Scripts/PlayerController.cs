6eba0441310e7b835a15d8b5e66f7284c1dd7af6
Calmbl4 <boss.buyakevich@mail.ru>
2023-02-03 02:08:44 +0300

    Comeback
    Управление добавил
    подрисовал карту но все ровно еще не то
    и стены подправил (слой)

--- 2.5D_Game7/Assets/Scripts/PlayerController.cs
+++ 2.5D_Game7/Assets/Scripts/PlayerController.cs
@@ -1,41 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 1f;
-    public float collisionOffset = 0.05f;
-    public ContactFilter2D movementFilter;
+    public Rigidbody2D rigidbody;   
+    public Animator animator;
 
-    Vector2 movementInput;
-    Rigidbody2D rigidbody;
-    List<RaycastHit2D> castCollision = new List<RaycastHit2D>();
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed = 1f;
+
+    public float x;
+    public float y;
+    private bool isWalking;
+
+    //public float collisionOffset = 0.05f;
+    //Vector2 movementInput;
+    private Vector2 moveDirection;
+
+
+
+    private void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        GetInput();
+        Animations();
 
+    }
     private void FixedUpdate()
     {
+        rigidbody.velocity = moveDirection * moveSpeed;
+    }
+    private void GetInput()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(x, y);
+        moveDirection.Normalize();
+    }
+    void Animations()
+    {
+        if (moveDirection.magnitude > 0.1f || moveDirection.magnitude < -0.1f)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+
+        if (isWalking)
+        {
+            animator.SetFloat("X", x);
+            animator.SetFloat("Y", y);
+        }
+        animator.SetBool("isMoving",isWalking);
+    }
+    void StopMoving()
+    {
+        rigidbody.velocity = new Vector2(0, 0);
+    }
     // Проверка которая позволяет сразу нажимать к примеру A и W что бы перс двигался а не замирал и упирался в стену  
+    /*void StabilizationMovementDiagonally()
+    {
         if (movementInput != Vector2.zero)
         {
-           bool success = TryMove(movementInput);
+            bool success = TryMove(movementInput);
             if (!success)
             {
                 success = TryMove(new Vector2(movementInput.x, 0));
                 if (!success)
                 {
-                    success = TryMove(new Vector2(0,movementInput.y));
+                    success = TryMove(new Vector2(0, movementInput.y));
                 }
             }
         }
     }
-
     private bool TryMove(Vector2 direction)
     {
         int count = rigidbody.Cast(direction, movementFilter, castCollision, moveSpeed * Time.fixedDeltaTime + collisionOffset);
@@ -49,9 +94,6 @@ public class PlayerController : MonoBehaviour
         {
             return false;
         }
-    }
-    void OnMove(InputValue movementValue)
-    {
-        movementInput = movementValue.Get<Vector2>();
-    }
+    }*/
+
 }
